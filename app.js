aba88096e68834ad6a19cb01ac74a6ce77b2f301
DiromaUmukoro <godbless.umukoro@yahoo.com>
2021-06-20 20:56:36 -0500

    website update

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/portfolio',(req,res)=>{
 })
 
 app.get('/resume', function (req, res) {
-    var filePath = "/files/GODBLESSUMUKORO.pdf";
+    var filePath = "/files/GODBLESS_UMUKORO.pdf";
 
     fs.readFile(__dirname + filePath , function (err,data){
         res.contentType("application/pdf");
